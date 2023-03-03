71d8ea5cb752471ce3b6dcba9ec04ccfc0d86e59
rohanD11 <105814846+rohanD11@users.noreply.github.com>
2023-03-03 16:07:28 +0530

    Added solution - LeetHub

---
+++ Binary Search - GFG/binary-search.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int f=0;
+        int l=n-1;
+        
+        while(f<=l){
+          int   mid=f+(l-f)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+             else if(k<arr[mid]){
+                 l=mid-1;
+             }
+             else 
+             f=mid+1;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
