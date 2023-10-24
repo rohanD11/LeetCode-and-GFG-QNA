71e159453d5f17db7ab9f9a3c3fad5681ec21fd5
rohanD11 <105814846+rohanD11@users.noreply.github.com>
2023-10-24 21:10:13 +0530

    Time: 20 ms (61.69%), Space: 13.4 MB (32.72%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string shortestSupersequence(string a, string b)
+{
+	// Write your code here.
+	int n=a.size();
+	int m=b.size();
+	vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			if(a[i-1]==b[j-1])dp[i][j]=dp[i-1][j-1]+1;
+			else{
+				dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+			}
+
+					}
+	}
+     string ans="";
+	 int i=n,j=m;
+	 while(i>0&&j>0){
+		 if(a[i-1]==b[j-1]){
+			 ans+=a[i-1];
+			 i--;
+			 j--;
+		 }
+		 else if(dp[i-1][j]>dp[i][j-1]){
+			 ans+=a[i-1];
+			 i--;
+		 }
+		 else {
+			 ans+=b[j-1];
+			 j--;
+
+		 }
+	 }
+	 while(i>0){
+		 ans+=a[i-1];
+		 i--;
+	 }
+	 while(j>0){ans+=b[j-1];
+	 
+	 j--;}
+	reverse(ans.begin(),ans.end());
+	return ans;
+
+
+}
+    string shortestCommonSupersequence(string str1, string str2) {
+        return shortestSupersequence(str1,str2);
+    }
+};
